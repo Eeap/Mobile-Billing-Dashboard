8f8066eba4cca9a538e06e83386c6dfce43d1f97
Eeap <kdg97811@naver.com>
2023-11-30 15:13:03 +0900

    refactor: comment out sample data

--- back-end/app/controllers/cost_explorer_controller.go
+++ back-end/app/controllers/cost_explorer_controller.go
@@ -6,6 +6,7 @@ import (
 	"main/app/services"
 )
 
+// @Router /v1/aws-resource [get]
 func GetResourcesCost(c *fiber.Ctx) error {
 	costIn := &models.CostExplorer{}
 	if err := c.QueryParser(costIn); err != nil {
@@ -15,6 +16,80 @@ func GetResourcesCost(c *fiber.Ctx) error {
 		})
 	}
 	resData, _ := services.GetCostUsageByResource(costIn)
+	//resData := []models.CostResource{
+	//	{
+	//		Key:       "Amazon Simple Storage Service",
+	//		Amount:    "0.8",
+	//		TimeEnd:   "2023-10-29",
+	//		TimeStart: "2023-10-30",
+	//	},
+	//	{
+	//		Key:       "Amazon Simple Storage Service",
+	//		Amount:    "1.0",
+	//		TimeEnd:   "2023-10-30",
+	//		TimeStart: "2023-10-31",
+	//	},
+	//	{
+	//		Key:       "Amazon Simple Storage Service",
+	//		Amount:    "1.8",
+	//		TimeEnd:   "2023-10-31",
+	//		TimeStart: "2023-11-01",
+	//	},
+	//	{
+	//		Key:       "Amazon Simple Storage Service",
+	//		Amount:    "2.8",
+	//		TimeEnd:   "2023-11-01",
+	//		TimeStart: "2023-11-02",
+	//	},
+	//	{
+	//		Key:       "Amazon Relational Database Service",
+	//		Amount:    "8.0",
+	//		TimeEnd:   "2023-10-29",
+	//		TimeStart: "2023-10-30",
+	//	},
+	//	{
+	//		Key:       "Amazon Relational Database Service",
+	//		Amount:    "12.592",
+	//		TimeEnd:   "2023-10-30",
+	//		TimeStart: "2023-10-31",
+	//	},
+	//	{
+	//		Key:       "Amazon Relational Database Service",
+	//		Amount:    "5.602",
+	//		TimeEnd:   "2023-10-31",
+	//		TimeStart: "2023-11-01",
+	//	},
+	//	{
+	//		Key:       "Amazon Relational Database Service",
+	//		Amount:    "10.235",
+	//		TimeEnd:   "2023-11-01",
+	//		TimeStart: "2023-11-02",
+	//	},
+	//	{
+	//		Key:       "Amazon Virtual Private Cloud",
+	//		Amount:    "3.0",
+	//		TimeEnd:   "2023-10-29",
+	//		TimeStart: "2023-10-30",
+	//	},
+	//	{
+	//		Key:       "Amazon Virtual Private Cloud",
+	//		Amount:    "2.592",
+	//		TimeEnd:   "2023-10-30",
+	//		TimeStart: "2023-10-31",
+	//	},
+	//	{
+	//		Key:       "Amazon Virtual Private Cloud",
+	//		Amount:    "5.602",
+	//		TimeEnd:   "2023-10-31",
+	//		TimeStart: "2023-11-01",
+	//	},
+	//	{
+	//		Key:       "Amazon Virtual Private Cloud",
+	//		Amount:    "4.235",
+	//		TimeEnd:   "2023-11-01",
+	//		TimeStart: "2023-11-02",
+	//	},
+	//}
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"totalResults": len(resData),
 		"resources":    resData,
